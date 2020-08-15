4144aee93cff0fe0cf757d6ad7bb269a74f1a312
RafaelCopes <rafaelcopes16@gmail.com>
2020-08-15 14:08:32 -0300

    Environment setup  and add theme and styles

---
+++ src/styles/theme.ts
@@ -0,0 +1,27 @@
+export default {
+  colors: {
+    primary: '#FF8700',
+    secundary: '#FF500F',
+    grey: '#8C91A5',
+    lightGrey: '#F5F6FA',
+    darkGrey: '#6C757D',
+    income: '#20C05C',
+    outcome: '#FD4F48',
+    background: '#FFF',
+  },
+  fontSizes: {
+    default: '1.4rem',
+    big: '1.6rem',
+    small: '1.2rem',
+    smallest: '1rem',
+  },
+  spacing: {
+    default: '1.6rem',
+    vertical: '1.6rem 0',
+    horizontal: '0 1.6rem',
+    big: '2.4rem'
+  },
+  transition: {
+    default: '180ms ease-in-out',
+  }
+} as const;
